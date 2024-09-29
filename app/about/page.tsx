b9f37a40a0d48ccef9238263a1d5b36e88c46de7
elvinafirmansyah <elvinafirmansyah@gmail.com>
2024-09-29 18:31:11 +0700

    update cv resume

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import { FaLongArrowAltRight } from "react-icons/fa";
 import Link from "next/link";
 import { FaInstagram, FaFacebookF, FaGithub, FaDribbble } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import { Button } from "@/components/ui/button"
 
 const inter = Inter({ subsets: ["latin"] });
 const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
@@ -11,20 +12,23 @@ const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 function About() {
 
   return (
-    <main className={`flex flex-col items-center justify-between px-7  md:px-32 py-24`}>
+    <main className={`flex flex-col items-center justify-between px-7 md:px-32 py-20`}>
       
       <div className="absolute z-[-1] left-1/4 before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-sky-300 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-sky-400 before:dark:opacity-10 after:dark:from-cyan-500 after:dark:via-[#68f0ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
       </div>
-      <div className="content w-full md:w-3/4 flex flex-col gap-y-7">
+      <div className="content w-full md:w-3/4 flex flex-col gap-y-6">
         <div className="flex flex-col gap-y-2">
           <h1 className={`font-bold text-6xl ${inter.className}`}>Hello, I am Elvina</h1>
           <h3 className={`font-semibold text-2xl text-sky-400 ${inter.className}`}>Full-Stack Developer & UI/UX Designer</h3>
         </div>
         <p className={`w-full text-lg`}>I&apos;m Elvina Firmansyah. I&apos;m a Full-Stack Developer and UI/UX Designer based in Indonesia. I&apos;m passionate about coding, developing and designing various websites as well as working on projects related to web development and UI/UX design. I&apos;ve been coding for the past 2 years and have enjoyed every moment of it. My main focus is to create accessible knowledge and digital experiences both in my career and for the future.</p>
+        <div className="flex flex-col gap-y-3">
+        <Button variant="default" className="w-fit text-md hover:bg-sky-400 hover:text-white bg-glass text-sky-400"><Link href='/assets/cv.pdf' target="_blank">Resume</Link></Button>
           <Link href='/projects' className='flex gap-x-2 items-center hover:underline hover:underline-offset-4'>
             See More Projects
             <FaLongArrowAltRight className='animation-link' />
           </Link>
+        </div>
         <div className="flex gap-x-5">
           <Link target='_blank' href='https://www.instagram.com/virn_el/'><FaInstagram className='text-xl hover:text-sky-400'  /></Link>
           <Link target='_blank' href='https://m.facebook.com/elvina.firmansyah.50'><FaFacebookF className='text-xl hover:text-sky-400' /></Link>
